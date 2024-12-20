b31e0d5249f79f6ec4f1f188f6c2acaf689b9678
nguyentu1112005 <nguyentu1112005@gmail.com>
2024-12-20 10:54:21 +0700

    firts commit

--- 06.php
+++ 06.php
@@ -1,12 +1,16 @@
 <?php
-    $conn = mysqli_connect('localhost', 'root', '', 'qlsv23s', '3306');
-    $sql = "SELECT * FROM sinhvien";
-    //Thực thi truy vấn
-    $kq = mysqli_query($conn, $sql);
+$conn = mysqli_connect('localhost', 'root', '', 'qlsv23s', '3306');
+$sql = "SELECT * FROM sinhvien";
+// Thực thi truy vấn
+$kq = mysqli_query($conn, $sql);
+
+// Tính tổng học bổng
+$total_hocbong = 0;
+while ($row = mysqli_fetch_assoc($kq)) {
+    $total_hocbong += $row['hocbong'];
+    $sinhviens[] = $row; // Lưu trữ kết quả để lặp lại trong bảng
+}
 ?>
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -19,7 +23,7 @@
 <body>
     <div class="container">
         <h1 class="text-center text-primary mt-3">DANH SÁCH SINH VIÊN</h1>
-        <table class="table">
+        <table class="table table-bordered">
             <thead>
                 <tr>
                     <th scope="col">Mã SV</th>
@@ -30,20 +34,22 @@
                 </tr>
             </thead>
             <tbody>
-                <?php while($row = mysqli_fetch_assoc($kq)) { ?>
+                <?php foreach ($sinhviens as $row) { ?>
                 <tr>
-                    <td><?php echo $row["masv"];  ?></td>
-                    <td><?php echo $row["hoten"];  ?></td>
-                    <td><?php echo $row["phai"] ? "Nam" : "Nữ";  ?></td>
-                    <td><?php echo date("d/m/Y", strtotime($row["ngaysinh"]));  ?></td>
-                    <td><?php echo $row["hocbong"];  ?></td>
+                    <td><?php echo $row["masv"]; ?></td>
+                    <td><?php echo $row["hoten"]; ?></td>
+                    <td><?php echo $row["phai"] ? "Nam" : "Nữ"; ?></td>
+                    <td><?php echo date("d/m/Y", strtotime($row["ngaysinh"])); ?></td>
+                    <td><?php echo number_format($row["hocbong"]); ?> VNĐ</td>
                 </tr>
                 <?php } ?>
+                <tr>
+                    <td colspan="4" class="text-right font-weight-bold">Tổng học bổng</td>
+                    <td class="font-weight-bold"><?php echo number_format($total_hocbong); ?> VNĐ</td>
+                </tr>
             </tbody>
         </table>
     </div>
-
-
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
